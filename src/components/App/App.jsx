134ccb085656074b2c14e11d3daa60a2dc7477d5
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2022-01-25 15:30:42 +0100

    creates slides from mock data

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Slider from '../Slider/Slider';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Slider></Slider>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+
+import { Carousel } from 'react-bootstrap';
+
 import slides from '../../mock/slides.json';
 
 const Slider = () => {
-  return <div>Slider</div>;
+  return (
+    <div>
+      <Carousel>
+        {slides.map(slide => {
+          const { img, text, label } = slide;
+          return (
+            <Carousel.Item>
+              <img className="d-block w-100" src={img} alt={label} />
+              <Carousel.Caption>
+                <h3>{label}</h3>
+                <p>{text}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+    </div>
+  );
 };
 
 export default Slider;
